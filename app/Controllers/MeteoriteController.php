4cf5929f2b8de639c0bf845eba033eadf4154667
Ariel Wazana <117109691+ArielWazProgramming@users.noreply.github.com>
2024-03-24 21:21:24 -0400

    Added RocketController.php & MeteoriteController.php

---
+++ app/Controllers/MeteoriteController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vanier\Api\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Vanier\Api\Models\MeteoriteModel;
+
+class MeteoriteController extends BaseController
+{
+    public MeteoriteModel $meteorite_model;
+
+    public function __construct()
+    {
+        $this->meteorite_model = new MeteoriteModel();
+    }
+
+    public function handleGetAllMeteorites(Request $request, Response $response, array $uri_args): Response
+    {
+        $meteorites_filters = $request->getQueryParams();
+        $this->meteorite_model->setPaginationOptions(
+            $request,
+            $meteorites_filters["page"] ?? 1,
+            $meteorites_filters["page_size"] ?? 15
+        );
+
+        $meteorites = $this->meteorite_model->getAllMeteorites($meteorites_filters);
+        $response = $this->makeResponse($response, $meteorites);
+        return $response;
+    }
+
+    public function handleGetMeteoriteById(Request $request, Response $response, array $uri_args): Response
+    {
+        $supplied_meteorite_id = $uri_args["meteorite_id"];
+        $this->validateIntId($request, $supplied_meteorite_id);
+
+        $meteorite = $this->meteorite_model->getMeteoriteById($supplied_meteorite_id);
+        $response = $this->makeResponse($response, $meteorite);
+        return $response;
+    }
+}
